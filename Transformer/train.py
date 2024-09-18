006a26e9338b9ba875df4015231e35460162cfdc
srikant2589 <73124214+srikant2589@users.noreply.github.com>
2024-09-18 07:21:08 +0530

    " validation model "

--- Transformer/train.py
+++ Transformer/train.py
@@ -14,6 +14,49 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import warnings
 
+def greedy_decode(model, source,source_mask, tokenizer_src,tokenizer_tgt,max_len,device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    encoder_output = model.encoder(source, source_mask)
+    decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1)==max_len:
+            break
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+        out = model.decode(encoder_output,source_mask,decoder_input,decoder_mask)
+
+        prob= model.project(out[:,-1])
+        _,next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat([decoder_input,torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)],dim=1)
+        if next_word == eos_idx:
+            break
+    return decoder_input.squeeze(0)
+
+def run_validation(model,validation_ds,tokenizer_src,tokenizer_tgt,max_len,device,print_msg,global_state, writer,num_exapmles=2):
+    model.eval()
+    count=0
+
+    console_width = 80 # size of the control window -- just use a defalut value
+    with torch.no_grad():
+        for batch in validation_ds:
+            count+=1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['enocoder_mask'].to(device)
+            assert encoder_input.size(0)==1 , ' batch size should be 1 for validation'
+            model_out = greedy_decode(model, encoder_input, encoder_mask,tokenizer_src,tokenizer_tgt,max_len,device)
+            source_text =batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            print_msg('-'*console_width)
+            print_msg(f'Source:{source_text}')
+            print_msg(f'Target:{target_text}')
+            print_msg(f'Predicted:{model_out_text}')
+
+            if count == num_exapmles:
+                break
+
 
 def get_all_sentences(ds,lang):
     for item in ds:
@@ -89,9 +132,10 @@ def train_model(config):
     loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id(['PAD']),label_smoothing=0.1).to(device)
 
     for epoch in range(initial_epoch, config['num_epochs']):
-        model.train()
+
         batch_iterator= tqdm(train_dataloader,desc =f'processing epoch {epoch:02d}')
         for batch in batch_iterator:
+            model.train()
             encoder_input = batch['encoder_input'].to(device) # (batch, seq_len)
             decoder_input = batch['decoder_input'].to(device) # (batch, seq_len)
             encoder_mask = batch['encoder_mask'].to(device) # (batch, 1,1,seq_len)
@@ -111,7 +155,10 @@ def train_model(config):
             optimizer.step()
             optimizer.zero_grad()
 
+
             global_step +=1
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device,
+                       lambda msg: batch_iterator.write(msg), global_step, writer)
 
         model_filename = get_weights_file_path(config,f"{epoch:02d}")
         torch.save({
@@ -120,6 +167,8 @@ def train_model(config):
             'optimizer_state_dict': optimizer.state_dict(),
             'global_step': global_step
         },model_filename)
+
+
 if __name__ =='__main__':
     warnings.filterwarnings('ignore')
     config= get_config()
